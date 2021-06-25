24580d82a301ea69c41a0eb4cfb151ae163e5a5c
Lj <tnedutsl457892@gmail.com>
2021-06-25 10:03:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,17 +1,14 @@
 class Application
 
-  def call(env)
-    resp = Rack::Response.new
-    time = Time.now
+def call(env)
+  resp = Rack::Response.new
+  time = Time.now
 
-    resp.write "#{time}"
-
-    if time < = 12
+    if time.hour < 12
       resp.write "Good Morning!"
-    else
-      resp.write "Good Afternoon!"
+    if time.hour > 12
+      resp.write "Good Afternoon"
     end
-
     resp.finish
   end
 
